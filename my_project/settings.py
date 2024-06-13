2a74c177a250e2c17f84ba7f3035f868574d3ff2
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-06-13 12:22:15 +0100

    Settings for heroku deploy

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kv01x%jr3p369tjq_=yfqtbo+sm5(7o5fce81%d_lju2cq25eu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
